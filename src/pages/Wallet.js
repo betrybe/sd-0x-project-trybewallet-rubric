51cc1e61e4eef51d50ed5d9e70eb7799fe9a9746
“brunobatista” <brunobatista101@gmail.com>
2021-02-01 16:23:51 -0300

    projeto pasando tudo

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Header from '../components/Header';
+import Form from '../components/Form';
+import Table from '../components/Table';
 
-class Wallet extends React.Component {
+export default class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <div>
+        <Header />
+        <Form />
+        <Table />
+      </div>
+    );
   }
 }
-
-export default Wallet;
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,32 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+// Esse reducer será responsável por tratar as informações da pessoa usuária
+import {
+  GET_CURRENCY, ADD_EXPENSE, REMOVE_EXPENSE,
+  EDIT_EXPENSE, EDIT_EXPENSE_DONE, CHANGE_EXPENSE,
+} from '../actions';
+
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+  editExpense: null,
+};
+
+export default function wallet(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case GET_CURRENCY: return { ...state, currencies: action.currency };
+  case ADD_EXPENSE: return { ...state,
+    expenses: [
+      ...state.expenses,
+      { id: state.expenses.length, ...action.expense.expenses },
+    ] };
+  case CHANGE_EXPENSE: return { ...state,
+    expenses: state.expenses.map((it) => (
+      it.id !== action.expense.id ? it : action.expense)),
+    editExpense: null };
+  case REMOVE_EXPENSE:
+    return { ...state,
+      expenses: state.expenses.filter((it) => it.id !== action.expense.id) };
+  case EDIT_EXPENSE: return { ...state, editExpense: action.expense };
+  case EDIT_EXPENSE_DONE: return { ...state, editExpense: null };
+  default: return state;
+  }
+}
