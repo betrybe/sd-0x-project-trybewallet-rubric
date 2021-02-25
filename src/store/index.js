ea07208f9162a038af8f26f37cde3efd91d60ad0
Leticia <leticia.bora@gmail.com>
2021-02-25 01:04:11 -0300

    trocando store

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,10 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../reducers';
 
-const composeEnhancer = (typeof window !== 'undefined'
-  && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
-
-const store = createStore(
-  rootReducer, composeEnhancer(applyMiddleware(thunk)),
-);
+const store = createStore(rootReducer, composeWithDevTools(
+  applyMiddleware(thunk),
+));
 
 export default store;
